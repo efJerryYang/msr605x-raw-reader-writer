f42e4a775499389f15cffd914dd6c8eaabb228c0
Jerry Yang <efjerryyang@outlook.com>
2024-09-12 06:47:50 +0800

    chore: revert the writer due to loop handling

--- build.bat
+++ build.bat
@@ -11,17 +11,17 @@ if %errorlevel% neq 0 (
 echo Compilation of reader.exe successful.
 
 echo Compiling writer.exe...
-g++ -std=c++11 -DWRITER_MAIN -o writer.exe msr_writer.cpp msr_common.o -L. -lMSR_API -luser32
+g++ -std=c++11 -DWRITER_MAIN -o writer.exe msr_writer.cpp -L. -lMSR_API -luser32
 if %errorlevel% neq 0 (
     echo Compilation of writer.exe failed.
     exit /b %errorlevel%
 )
 echo Compilation of writer.exe successful.
 
-echo Compiling tester.exe...
-g++ -std=c++11 -o tester.exe msr_tester.cpp msr_reader.cpp msr_writer.cpp msr_common.o -L. -lMSR_API -luser32
-if %errorlevel% neq 0 (
-    echo Compilation of tester.exe failed.
-    exit /b %errorlevel%
-)
-echo Compilation of tester.exe successful.
+@REM echo Compiling tester.exe...
+@REM g++ -std=c++11 -o tester.exe msr_tester.cpp msr_reader.cpp msr_writer.cpp msr_common.o -L. -lMSR_API -luser32
+@REM if %errorlevel% neq 0 (
+@REM     echo Compilation of tester.exe failed.
+@REM     exit /b %errorlevel%
+@REM )
+@REM echo Compilation of tester.exe successful.
